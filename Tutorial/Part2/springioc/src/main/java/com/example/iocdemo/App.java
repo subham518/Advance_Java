78452424a40d457e7af8735489f28b06cc3c3afd
Subham Kumar Padhy <presentpastfuture19@gmail.com>
2026-05-04 15:33:56 +0530

    Add files via upload

---
+++ Tutorial/Part2/springioc/src/main/java/com/example/iocdemo/App.java
@@ -0,0 +1,27 @@
+package com.example.iocdemo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Student s = (Student) context.getBean("studentBean");
+
+        s.display();
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
+
+    }
+}
